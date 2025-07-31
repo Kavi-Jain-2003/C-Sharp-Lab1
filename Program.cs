791fd44ee9c19899ef8d90a381794316e5d5092f
Kavi-Jain-2003 <kavijain220803@gmail.com>
2025-08-01 01:11:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assignments a1 = new Assignments();
+            a1.Sum();
+        }
+    }
+}
